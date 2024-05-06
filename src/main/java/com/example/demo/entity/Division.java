b24ef8843992abc0c3f3ded9b4faf78c017d883c
lindavataksi <your.email@example.com>
2024-05-05 23:57:27 -0400

    Added code to the entity package based off the UML diagram

---
+++ src/main/java/com/example/demo/entity/Division.java
@@ -0,0 +1,48 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
+import java.util.Set;
+
+@Entity
+@Table(name = "divisions")
+@Getter
+@Setter
+public class Division {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "division_id")
+    private Long id;
+
+    @Column(name = "division")
+    private String division_name;
+
+    @Column(name = "create_date")
+    @CreationTimestamp
+    private Date create_date;
+
+    @Column(name = "last_update")
+    @UpdateTimestamp
+    private Date last_update;
+
+    @ManyToOne
+    @JoinColumn(name = "country_id", insertable = false, updatable = false, nullable = false)
+    private Country country;
+
+//    private Long country_id;
+
+    @OneToMany(mappedBy = "division", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Set<Customer> customers;
+
+    @Column (name = "Country_id")
+    private Long country_id;
+    public void setCountry(Country country) {
+        setCountry_id(country.getId());
+        this.country = country;
+    }
+}
