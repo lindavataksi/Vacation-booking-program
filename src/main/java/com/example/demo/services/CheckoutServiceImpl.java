251b7a6b03d08ffdbb18d5dcc4cf324df2f8d3d5
lindavataksi <your.email@example.com>
2024-05-07 22:31:59 -0400

    add checkout service and checkoutserviceimpl java classes to services package

---
+++ src/main/java/com/example/demo/services/CheckoutServiceImpl.java
@@ -0,0 +1,43 @@
+package com.example.demo.services;
+
+import com.example.demo.dao.CartItemRepository;
+import com.example.demo.dao.CartRepository;
+import com.example.demo.entity.Cart;
+import com.example.demo.entity.CartItem;
+import com.example.demo.entity.StatusType;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.UUID;
+
+@Service
+public class CheckoutServiceImpl implements CheckoutService{
+    private CartRepository cartRepository;
+
+    // private CartItemRepository cartItemRepository;
+
+    public CheckoutServiceImpl(CartRepository cartRepository, CartItemRepository cartItemRepository){
+        this.cartRepository = cartRepository;
+        // this.cartItemRepository = cartItemRepository;
+    }
+
+    @Override
+    @Transactional
+    public PurchaseResponse placeOrder(Purchase purchase) {
+        Cart cart = purchase.getCart();
+//        Customer customer = purchaseData.getCustomer();
+
+        cart.setStatus(StatusType.ordered);
+        String orderTrackingNumber = UUID.randomUUID().toString();
+        cart.setOrderTrackingNumber(orderTrackingNumber);
+
+        Set<CartItem> cartItems = purchase.getCartItems();
+        cartItems.forEach(cartItem -> cartItem.setCart(cart));
+
+        cartRepository.save(cart);
+        return new PurchaseResponse(orderTrackingNumber);
+    }
+
+}
+
